6ceb2918dbd07a9f128fea869ae08a80d4f3a7a2
sebiny12p <sebinylabs@gmail.com>
2026-06-28 03:33:52 +0300

    Delete Wardle - Linux Installer directory

--- Wardle - Linux Installer/Wardle - Windows Installer/install_launcher.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Force the script to run from the current folder
-cd /d "%~dp0"
-
-:: Run the PowerShell script located in this specific folder
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0wardle_setup.ps1"
-
-PAUSE
